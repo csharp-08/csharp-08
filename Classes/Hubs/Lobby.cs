752619d225a448864fdb564bb599104c50d91799
Victor Paltz <paltz.victor@free.fr>
2019-10-01 17:00:34 +0200

    remose user when deconnecting

--- Classes/Hubs/Lobby.cs
+++ Classes/Hubs/Lobby.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace csharp_08
 {
     public class Lobby : Hub
     {
-        public static List<User> drawers = new List<User>();
+        public static Dictionary<string, User> drawers = new Dictionary<string, User>();
         public static Canvas canvas = new Canvas();
 
         public override async Task OnConnectedAsync()
@@ -16,12 +17,21 @@ namespace csharp_08
             string id = Context.ConnectionId;
             string username = Context.GetHttpContext().Request.Query["username"];
 
-            drawers.Add(new User(id, username));
+            drawers[id] = new User(id, username);
+
             Debug.WriteLine(drawers.Count);
 
             Debug.WriteLine($"{username} is connected!");
             await base.OnConnectedAsync();
 
+            await Clients.All.SendAsync("drawers", JsonConvert.SerializeObject(drawers));
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            string id = Context.ConnectionId;
+            await base.OnDisconnectedAsync(exception);
+            drawers.Remove(id);
             await Clients.All.SendAsync("drawers", JsonConvert.SerializeObject(drawers));
         }
     }

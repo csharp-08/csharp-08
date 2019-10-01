278743e35924a40bb43097b3338c57273d93e74c
Mayeul Blanzat <may-26@hotmail.fr>
2019-10-01 12:20:49 +0200

    Feature: Create Shape class' attributes

--- Shape.cs
+++ Shape.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace csharp_08
 {
     public abstract class Shape
     {
+        private static uint IDs = 0;
+        public static uint ID { get; private set; }
+        public static List<double> Vertices { get; private set; }
+        public static int Thickness { get; private set; }
+        public static Color ShapeColor { get; private set; }
+        public static User User { get; private set; }
+        
+        public Shape()
+        {
+            ID = IDs;
+            IDs++;
+        }
     }
 }

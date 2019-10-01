caffe0d743830f9c6c561303cd8d26e01b21ae9b
Mayeul Blanzat <may-26@hotmail.fr>
2019-10-01 16:24:35 +0200

    Feature: New classe Pencil

---
+++ Classes/Shapes/Pencil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace csharp_08
+{
+    public class Pencil : Shape
+    {
+        public Pencil(List<Tuple<double, double>> Vertices, User Owner, int Thickness = 1, Color Color = new Color()) :
+               base(Vertices, Owner, Thickness, Color)
+        {
+        }
+    }
+}

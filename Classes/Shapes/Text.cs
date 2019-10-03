28146e020dd3f31a9fbd18c17548120c42a8ae18
Mayeul Blanzat <may-26@hotmail.fr>
2019-10-03 14:17:14 +0200

    Feature: Add Text class

---
+++ Classes/Shapes/Text.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using Newtonsoft.Json;
+
+namespace csharp_08.Classes.Shapes
+{
+    public class Text : Shape
+    {
+        public string InnerText;
+        public Text(List<Tuple<double, double>> Vertices, User Owner, string Text, int Thickness = 1, Color Color = new Color()) :
+               base(Vertices, Owner, Thickness, Color)
+        {
+            InnerText = Text;
+        }
+
+        public override string ToString()
+        {
+            return "Forme: Text\nText: " + Text + "\n" + base.ToString();
+        }
+
+        public override string Draw()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}

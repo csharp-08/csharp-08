fcadb2c66efed6d75d13e0c959ceb065387f58dd
Victor Paltz <paltz.victor@free.fr>
2019-11-25 18:57:34 +0100

    Documentation fo ShapeConfig class

--- Classes/Shapes/ShapeConfig.cs
+++ Classes/Shapes/ShapeConfig.cs
@@ -2,6 +2,10 @@
 
 namespace csharp_08
 {
+    /// <summary>
+    /// An object that store the graphical configuration of a shape.
+    /// It stores the position, the shape, the color, and many other parameters.
+    /// </summary>
     public class ShapeConfig
     {
         public int Thickness { get; private set; }
@@ -14,6 +18,18 @@ namespace csharp_08
         public double Rotation { get; private set; }
         public bool IsEmpty { get; private set; }
 
+        /// <summary>
+        /// Constructor of the ShapeConfig object.
+        /// </summary>
+        /// <param name="Thickness">The thickness of the shape's lines</param>
+        /// <param name="Color">The color of the shape</param>
+        /// <param name="BorderColor">The color of the border of the shape</param>
+        /// <param name="OffsetX">The position on the x-axis of the shape on the canvas</param>
+        /// <param name="OffsetY">The position on the y-axis of the shape on the canvas</param>
+        /// <param name="ScaleX">The scale of x-axis (scale = 1.0 is normal, scale = 2.0 stretches the shape of a factor 2)</param>
+        /// <param name="ScaleY">The scale of y-axis (scale = 1.0 is normal, scale = 2.0 stretches the shape of a factor 2)</param>
+        /// <param name="Rotate">The rotation degree of the shape</param>
+        /// <param name="IsEmpty">Tells if the object is empty inside or not</param>
         public ShapeConfig(int Thickness = 10, Color Color = new Color(), Color BorderColor = new Color(),
                            double OffsetX = 0, double OffsetY = 0, double ScaleX = 1,
                            double ScaleY = 1, double Rotate = 0, bool IsEmpty = false)
@@ -29,8 +45,15 @@ namespace csharp_08
             this.IsEmpty = IsEmpty;
         }
 
+        /// <summary>
+        /// Default configuration of style for a shape.
+        /// </summary>
         public static ShapeConfig DefaultConfig { get; } = new ShapeConfig(10, new Color(), Color.Black, 0, 0, 1, 1, 0, false);
 
+        /// <summary>
+        /// ToString function which describes the object with a string
+        /// </summary>
+        /// <returns>String describing the object</returns>
         public override string ToString()
         {
             string str = $"Congig(Thickness: {Thickness}, Color: {Color}, BorderColor: {BorderColor}, OffsetX: {OffsetX}, ";
